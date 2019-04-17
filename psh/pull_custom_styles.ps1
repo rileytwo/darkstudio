34088b46f53b622be79e98fe77259490a29e2ef5
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-04-17 09:51:06 -0500

    updated .gitignore to hide ps1

---
+++ psh/pull_custom_styles.ps1
@@ -0,0 +1,4 @@
+$Source = 'C:\Program Files\RStudio\www\custom_styles.css'
+$Dest = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Desktop\Projects\Github\daRkstudio\'
+
+Copy-Item -Path $Source -Destination $Dest -Force -Verbose
