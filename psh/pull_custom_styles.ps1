e447c4c9d32b53dce0a4ad8db08930798323364a
Riley Roach <rroach@imodules.com>
2019-05-02 15:09:33 -0500

    update .gitignore to *hopefully* hide psh

--- psh/pull_custom_styles.ps1
+++
@@ -1,4 +0,0 @@
-$Source = 'C:\Program Files\RStudio\www\custom_styles.css'
-$Dest = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Desktop\Projects\Github\daRkstudio\'
-
-Copy-Item -Path $Source -Destination $Dest -Force -Verbose
