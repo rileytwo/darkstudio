e11035bde87974dda15c8eede5ad73840c65f092
rileytwo <34988536+rileytwo@users.noreply.github.com>
2020-05-26 20:47:31 -0500

    use single link function, create_darkstudio_link() rather than separate functions

--- R/link.R
+++ R/link.R
@@ -2,12 +2,11 @@
 # utils.R
 # Created by Riley Roach on 2020-05-25
 #
-link_default <- function() {
-  link <- '<link rel="stylesheet" href="darkstudio.css" type="text/css"/>'
-  return(link)
-}
-
-link_custom <- function(href = NULL) {
-  link <- paste0('<link rel="stylesheet" href="', href,'" type="text/css"/>')
+create_darkstudio_link <- function(href = NULL) {
+  if (length(href) != 0) {
+    link <- paste0('<link rel="stylesheet" href="', href,'" type="text/css"/>')
+  } else {
+    link <- '<link rel="stylesheet" href="darkstudio.css" type="text/css"/>'
+  }
   return(link)
 }
