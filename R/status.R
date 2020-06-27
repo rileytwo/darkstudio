2cc5d0e8d8dc3d7707443348294f48ab92fd6279
rileytwo <34988536+rileytwo@users.noreply.github.com>
2020-06-27 15:06:08 -0500

    new status function to provide a simple sitrep of darkstudio

---
+++ R/status.R
@@ -0,0 +1,27 @@
+#'
+#' status.R
+#' Created by Riley Roach on 2020-06-27
+#' @keywords internal
+status <- function(path = NULL) {
+  darkstudio_status <- list(
+    dir_status = "",
+    index_status = "",
+    css_status = ""
+  )
+  if (!darkstudio:::settings_dir_exists(path = path)) {
+    darkstudio_status$dir_status <- paste(
+      "No darkstudio directory exists, or one could not be found.",
+      "If you know there is a directory, provide the path to ",
+      "darkstudio::status().",
+      "For example: darkstudio::status(path = <path_to_dir>)"
+    )
+  } else {
+    darkstudio_status$dir_status <- darkstudio:::settings_dir_exists(
+      path = path,
+      value = TRUE
+    )
+  }
+
+  return(darkstudio_status)
+
+}
